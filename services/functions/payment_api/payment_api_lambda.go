e275f0d25f5578016d40304528507d23ae23208d
NicoCodes13 <nickcar13@gmail.com>
2023-08-24 18:39:50 -0500

    fixing wrong references for the lambdas

--- services/functions/payment_api/payment_api_lambda.go
+++ services/functions/payment_api/payment_api_lambda.go
@@ -3,7 +3,7 @@ package main
 import "github.com/aws/aws-lambda-go/lambda"
 
 func hello() (string, error) {
-	return "hello λ!, I'm the payment ", nil
+	return "hello λ!, I'm the payment api", nil
 }
 
 func main() {
