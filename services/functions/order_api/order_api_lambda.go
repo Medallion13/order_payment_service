11736375cd9e34d81c46e805d66f2b4b58d88432
NicoCodes13 <nickcar13@gmail.com>
2023-08-26 17:00:01 -0500

    first version of lambda only retrives the send information

--- services/functions/order_api/order_api_lambda.go
+++ services/functions/order_api/order_api_lambda.go
@@ -1,11 +1,54 @@
 package main
 
-import "github.com/aws/aws-lambda-go/lambda"
+import (
+	"encoding/json"
+	"net/http"
 
-func hello() (string, error) {
-	return "hello λ!, I'm the order API", nil
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type CreateOrderRequest struct {
+	UserId     string `json:"user_id"`
+	Item       string `json:"item"`
+	Quantity   int    `json:"quantity"`
+	TotalPrice int64  `json:"total_price"`
+}
+
+type CreateOrderEvent struct {
+	OrderID    string `json:"order_id"`
+	TotalPrice int64  `json:"total_price"`
+}
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var order CreateOrderRequest
+
+	// Creating a response and adding headers
+	response := events.APIGatewayProxyResponse{
+		Headers: map[string]string{
+			"Content-Type":                 "application/json",
+			"Access-Control-Allow-Methods": "POST",
+		},
+	}
+
+	// unserialize the event and save the requiered information in the struc object
+	err := json.Unmarshal([]byte(request.Body), &order)
+	if err != nil {
+		response.StatusCode = http.StatusBadRequest
+		return response, err
+	}
+
+	body, err := json.Marshal(CreateOrderEvent{OrderID: "ramdomId1", TotalPrice: order.TotalPrice})
+	if err != nil {
+		response.StatusCode = http.StatusBadRequest
+		return response, err
+	}
+
+	response.StatusCode = http.StatusOK
+	response.Body = string(body)
+	return response, nil
 }
 
 func main() {
-	lambda.Start(hello)
+	lambda.Start(handler)
 }
